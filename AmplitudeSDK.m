f18e0f1fd1abee78922a1fc16fdcd05f19f321da
Farcas Bianca <farcas.bianca25@gmail.com>
2017-03-08 10:43:25 +0200

    Added logOutUser method and modified logRevenue

--- AmplitudeSDK.m
+++ AmplitudeSDK.m
@@ -6,38 +6,47 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(initialize:(NSString* )writeKey)
-{
-  [Amplitude instance].trackingSessionEvents = YES;
-  [[Amplitude instance] initializeApiKey: writeKey];
+RCT_EXPORT_METHOD(initializeApiKey:(NSString *)apiKey) {
+    [[Amplitude instance] setTrackingSessionEvents:YES];
+    [[Amplitude instance] initializeApiKey:apiKey];
 }
 
-RCT_EXPORT_METHOD(setUserId:(NSString *)userId)
-{
-  [[Amplitude instance] setUserId:userId];
+RCT_EXPORT_METHOD(setUserId:(NSString *)userId) {
+    [[Amplitude instance] setUserId:userId];
 }
 
-RCT_EXPORT_METHOD(setUserProperties:(NSDictionary *)properties)
-{
-  [[Amplitude instance] setUserProperties:properties];
+RCT_EXPORT_METHOD(setUserProperties:(NSDictionary *)properties) {
+    [[Amplitude instance] setUserProperties:properties];
 }
 
-RCT_EXPORT_METHOD(logEvent:(NSString *)event)
-{
-  [[Amplitude instance] logEvent: event];
+RCT_EXPORT_METHOD(clearUserProperties) {
+    [[Amplitude instance] clearUserProperties];
 }
 
-RCT_EXPORT_METHOD(logEventWithProps:(NSString *)eventKey properties:(NSDictionary *)properties)
-{
-  [[Amplitude instance] logEvent: eventKey withEventProperties: properties];
+RCT_EXPORT_METHOD(logEventWithProperties:(NSString *)event withProperties:(NSDictionary *)properties) {
+    if(properties){
+        [[Amplitude instance] logEvent:event withEventProperties:properties];
+    }
+    else [[Amplitude instance] logEvent:event withEventProperties:nil];
+    
 }
 
-RCT_EXPORT_METHOD(logRevenue:(NSString *)productIdentifier quantity:(int)quantity price:(double)price receipt:(NSString*)receipt)
+RCT_EXPORT_METHOD(logEvent:(NSString *)event) {
+    
+    [[Amplitude instance] logEvent:event withEventProperties:nil];
+    
+}
+
+RCT_EXPORT_METHOD(logOutUser) {
+    [[Amplitude instance] setUserId:nil];  // not string nil
+}
+
+
+RCT_EXPORT_METHOD(logRevenue:(NSDictionary *)properties price:(double)price quantity:(int)quantity)
 {
-  AMPRevenue *revenue = [[[AMPRevenue revenue] setProductIdentifier:productIdentifier] setQuantity: quantity];
-  [revenue setPrice:[NSNumber numberWithDouble:price]];
-  [revenue setReceipt:[receipt dataUsingEncoding:NSUTF8StringEncoding]];
-  [[Amplitude instance] logRevenueV2:revenue];
+    AMPRevenue *revenue = [[[AMPRevenue revenue] setQuantity: quantity] setEventProperties:properties];
+    [revenue setPrice:[NSNumber numberWithDouble:price]];
+    [[Amplitude instance] logRevenueV2:revenue];
 }
 
 @end
